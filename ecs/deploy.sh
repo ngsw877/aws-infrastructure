0221001cf1a481f7a30e1c831df940a399fe8513
ngsw877 <otsda1055@yahoo.co.jp>
2024-01-11 19:02:58 +0900

    add: ECS on EC2スポットインスタンスとAutoScalingのテンプレート大枠を作成

--- ecs/deploy.sh
+++ ecs/deploy.sh
@@ -6,6 +6,20 @@ if [[ "$2" = "fargate" ]]; then
   STACK_NAME="sample-fargate"
   TEMPLATE="fargate.yml"
   PARAMETERS_FILE="fargate.json"
+
+elif [[ "$2" = "spot-instance" ]]; then
+  STACK_NAME="sample-spot-instance"
+  TEMPLATE="spot-instance.yml"
+  PARAMETERS_FILE="spot-instance.json"
+
+else
+  echo "ERROR: 第2引数が不正です。"
+  exit 1
+fi
+
+# 引数がある場合、STACK_NAMEに代入する
+if [[ "$3" != "" ]]; then
+  STACK_NAME="$3"
 fi
 
 aws cloudformation \
