100f301fee2c00e42c3ecaf33a4ab5055dd82368
ngsw877 <otsda1055@yahoo.co.jp>
2024-01-31 16:31:20 +0900

    update: SSMパラメータ登録の際、既存のパラメータが存在するかチェックしてもし存在すれば上書きするか選択できる処理を追加

--- ssm/put-parameter.sh
+++ ssm/put-parameter.sh
@@ -5,8 +5,30 @@ KEY="$2"
 VALUE="$3"
 
 aws ssm \
-  put-parameter \
+  get-parameter \
   --profile "${PROFILE}" \
   --name "/${KEY}" \
-  --value "${VALUE}" \
-  --type SecureString
+  1>/dev/null 2>&1
+
+# 254 = ParameterNotFound
+if [ $? -eq 254 ]; then
+  aws ssm \
+    put-parameter \
+    --profile "${PROFILE}" \
+    --name "/${KEY}" \
+    --value "${VALUE}" \
+    --type SecureString
+else
+  echo "パラメータが既に存在します。"
+  read -p "既存のパラメータを上書きしますか？(y/n): " overwrite
+  if [[ $overwrite == "y" ]]; then
+    aws ssm put-parameter \
+      --profile "${PROFILE}" \
+      --name "/${KEY}" \
+      --value "${VALUE}" \
+      --type SecureString \
+      --overwrite
+  else
+    echo "パラメータの上書きはキャンセルされました。"
+  fi
+fi
