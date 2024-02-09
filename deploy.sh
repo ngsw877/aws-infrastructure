53216220e0791fd0126b72d0d9c976a862a97cf3
ngsw877 <otsda1055@yahoo.co.jp>
2024-02-09 19:31:44 +0900

    update: デプロイスクリプトを改修

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-echo "profileを指定してください"
-read -r PROFILE
+# 引数の数をチェック
+if [ "$#" -eq 0 ]; then
+  echo "profileを指定してください"
+  read -r PROFILE
 
-echo "デプロイするCFnテンプレートのパスを指定してください"
-read -r TEMPLATE
+  echo "デプロイするCFnテンプレートのパスを指定してください"
+  read -r TEMPLATE
 
-echo "スタック名を指定してください"
-read -r STACK_NAME
+  echo "スタック名を指定してください"
+  read -r STACK_NAME
+else
+  # コマンドライン引数から値を割り当て
+  PROFILE=$1
+  TEMPLATE=$2
+  STACK_NAME=$3
+fi
 
+# AWS CloudFormationを使用してデプロイ
 aws cloudformation \
   --profile "${PROFILE}" \
   deploy \
