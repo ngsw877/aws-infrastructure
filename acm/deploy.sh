d5ed53da71f27e4d33e09f9e238f77d2a251dde0
ngsw877 <otsda1055@yahoo.co.jp>
2024-02-10 15:43:03 +0900

    update: ACMデプロイスクリプトを改修

--- acm/deploy.sh
+++ acm/deploy.sh
@@ -5,11 +5,19 @@ STACK_NAME="$2"
 TEMPLATE="acm.yml"
 PARAMETERS_FILE="acm.json"
 
-aws cloudformation \
-  deploy \
+echo "バージニアリージョンを利用しますか？ (y/n)"
+read -r USE_VIRGINIA
+if [[ "$USE_VIRGINIA" = "y" ]]; then
+  REGION="us-east-1"
+else
+  REGION="ap-northeast-1"
+fi
+
+aws cloudformation deploy \
   --profile "${PROFILE}" \
-  --template-file ${TEMPLATE} \
-  --stack-name ${STACK_NAME} \
+  --template-file "${TEMPLATE}" \
+  --stack-name "${STACK_NAME}" \
   --parameter-overrides "file://${PARAMETERS_FILE}" \
+  --region "${REGION}" \
   --capabilities CAPABILITY_NAMED_IAM \
   --no-execute-changeset
