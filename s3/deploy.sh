2936afd3d923efd8603a969bd077cd7590c5bc81
ngsw877 <otsda1055@yahoo.co.jp>
2024-02-12 14:39:00 +0900

    add: パラメータファイルの作成

--- s3/deploy.sh
+++ s3/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 PROFILE=$1
-TEMPLATE=$2
+TEMPLATE_FILE=$2
 STACK_NAME=$3
-BUCKET_NAME=$4
+
+# TEMPLATE_FILEの値から".yml"を".json"に置換してPARAMETERS_FILE変数を作成
+PARAMETERS_FILE="${TEMPLATE_FILE/.yml/.json}"
 
 aws cloudformation deploy \
   --profile "${PROFILE}" \
-  --template-file "${TEMPLATE}" \
+  --template-file "${TEMPLATE_FILE}" \
   --stack-name "${STACK_NAME}"  \
-  --parameter-overrides BucketName="${BUCKET_NAME}" \
+  --parameter-overrides "file://parameters/${PARAMETERS_FILE}" \
   --capabilities CAPABILITY_NAMED_IAM \
   --no-execute-changeset
