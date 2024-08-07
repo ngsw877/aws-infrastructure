865a33291b6ad109777cafbd03d5edfdac16000c
ngsw877 <otsda1055@yahoo.co.jp>
2024-08-07 10:24:57 +0900

    add: パブリックサブネット上にEC2インスタンスを作成

--- CDK/cdk-sample/lib/cdk-sample-stack.ts
+++ CDK/cdk-sample/lib/cdk-sample-stack.ts
@@ -1,19 +1,37 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+// ファイルを読み込むためのパッケージを import
+import { readFileSync } from "fs";
 
 export class CdkSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkSampleQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const vpc = new ec2.Vpc(this, "BlogVpc", {
+      ipAddresses:  ec2.IpAddresses.cidr('10.0.0.0/16'),
     });
 
-    const topic = new sns.Topic(this, 'CdkSampleTopic');
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC }
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // user-data.sh を読み込み、変数に格納
+    const script = readFileSync('./lib/resources/user-data.sh' , "utf8");
+    webServer1.addUserData(script);
+
+    // port80, 全ての IP アドレスからのアクセスを許可
+    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+    // EC2 インスタンスアクセス用の IP アドレスを出力
+    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
+      value: `http://${webServer1.instancePublicIp}`
+    });
   }
 }
