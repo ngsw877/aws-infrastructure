952e25cfa5bc205417b13ecf9099ae37701b8125
ngsw877 <otsda1055@yahoo.co.jp>
2024-09-02 09:23:26 +0900

    refactor

--- CDK/workshop/cdk-sample/lib/cdk-sample-stack.ts
+++ CDK/workshop/cdk-sample/lib/cdk-sample-stack.ts
@@ -3,8 +3,7 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as elbv2targets from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
-
+import * as targets from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
 // ファイルを読み込むためのパッケージを import
 import { readFileSync } from "fs";
 
@@ -14,23 +13,8 @@ export class CdkSampleStack extends Stack {
 
     // VPC を作成
     const vpc = new ec2.Vpc(this, "BlogVpc", {
-      ipAddresses:  ec2.IpAddresses.cidr('10.0.0.0/16'),
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
     });
-
-    // ALB を作成
-    const alb = new elbv2.ApplicationLoadBalancer(this, "WordPressALB", {
-      vpc,
-      internetFacing: true,
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC }
-    });
-
-    alb.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
-
-    // ALB のリスナーを作成
-    const listener = alb.addListener('Listener', {
-      port: 80
-    });
-
 
     // WebServer インスタンスを作成
     const webServer1 = new ec2.Instance(this, "WordpressServer1", {
@@ -39,24 +23,12 @@ export class CdkSampleStack extends Stack {
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
       }),
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
     });
 
     // user-data.sh を読み込み、変数に格納
     const script = readFileSync('./lib/resources/user-data.sh' , "utf8");
     webServer1.addUserData(script);
-
-    webServer1.connections.allowFrom(alb, ec2.Port.tcp(80));
-
-    const instanceTarget = new elbv2targets.InstanceTarget(webServer1);
-
-    // ALB のリスナーのターゲットとしてWebServerインスタンスを登録
-    listener.addTargets('Target', {
-      port: 80,
-      healthCheck: {
-        path: '/wp-includes/images/blank.gif'
-      },
-      targets: [instanceTarget]
-    });
 
     // RDS インスタンスを作成
     const dbServer = new rds.DatabaseInstance(this, "WordPressDB", {
@@ -69,5 +41,27 @@ export class CdkSampleStack extends Stack {
     // WebServer からのアクセスを許可
     dbServer.connections.allowDefaultPortFrom(webServer1);
 
+    // ALB を作成
+    const alb = new elbv2.ApplicationLoadBalancer(this, "LoadBalancer", {
+      vpc,
+      internetFacing: true,
+    });
+
+    // ALB のリスナーを作成
+    const listener = alb.addListener('Listener', {
+      port: 80
+    });
+
+    // ALB のリスナーのターゲットとしてWebServerインスタンスを登録
+    listener.addTargets('ApplicationFleet', {
+      port: 80,
+      targets: [new targets.InstanceTarget(webServer1, 80)],
+      healthCheck: {
+        path: '/wp-includes/images/blank.gif'
+      },
+    });
+
+    // ALB からインスタンスへのアクセスを許可
+    webServer1.connections.allowFrom(alb, ec2.Port.tcp(80));
   }
 }
