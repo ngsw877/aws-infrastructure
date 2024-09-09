a3d2de96217c4c2d587d9a1c188783dc7a2df5d9
ngsw877 <otsda1055@yahoo.co.jp>
2024-09-10 08:40:20 +0900

    add: workshop用のCDKプロジェクトを新規作成し、Lambdaを追加

---
+++ CDK/workshop/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -0,0 +1,17 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { Construct } from 'constructs';
+
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // defines an AWS Lambda resource
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_18_X,  // execution environment
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler" // file is "hello", function is "handler"
+    });
+
+  }
+}
