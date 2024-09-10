0391fd93bbc8c5c546fa806c7156f2d6d3b13a02
ngsw877 <otsda1055@yahoo.co.jp>
2024-09-10 09:05:01 +0900

    add: API Gateway追加

--- CDK/workshop/cdk-workshop/lib/cdk-workshop-stack.ts
+++ CDK/workshop/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway'
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,6 +13,11 @@ export class CdkWorkshopStack extends Stack {
       code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
       handler: "hello.handler" // file is "hello", function is "handler"
     });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    })
 
   }
 }
