9f4e9338a5841b6fba44b9e376255cb657263c57
ngsw877 <otsda1055@yahoo.co.jp>
2024-09-14 22:13:37 +0900

    add: ヒットカウンター用のLambdaとDynamoDBテーブルを追加

--- CDK/workshop/cdk-workshop/lib/cdk-workshop-stack.ts
+++ CDK/workshop/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway'
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,14 +10,18 @@ export class CdkWorkshopStack extends Stack {
 
     // defines an AWS Lambda resource
     const hello = new Function(this, "HelloHandler", {
-      runtime: Runtime.NODEJS_18_X,  // execution environment
+      runtime: Runtime.NODEJS_18_X, // execution environment
       code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
-      handler: "hello.handler" // file is "hello", function is "handler"
+      handler: "hello.handler", // file is "hello", function is "handler"
+    });
+
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello,
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
     const gateway = new LambdaRestApi(this, "Endpoint", {
-      handler: hello,
+      handler: helloWithCounter.handler,
     })
 
   }
