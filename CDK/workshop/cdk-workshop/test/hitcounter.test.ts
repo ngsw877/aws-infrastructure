fb40673d52861770e2956031fda064c641981925
ngsw877 <otsda1055@yahoo.co.jp>
2024-09-15 20:25:26 +0900

    refactor

--- CDK/workshop/cdk-workshop/test/hitcounter.test.ts
+++ CDK/workshop/cdk-workshop/test/hitcounter.test.ts
@@ -1,67 +1,69 @@
-import { Stack } from "aws-cdk-lib";
-import { Template, Capture } from "aws-cdk-lib/assertions";
-import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
-import { HitCounter } from "../lib/hitcounter";
+import {Stack} from "aws-cdk-lib";
+import {Template, Capture} from "aws-cdk-lib/assertions";
+import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
+import {HitCounter} from "../lib/hitcounter";
 
-test("DynamoDB Table Created With Encryption", () => {
-    const stack = new Stack();
-    // WHEN
-    new HitCounter(stack, "MyTestConstruct", {
-        downstream: new Function(stack, "TestFunction", {
-            runtime: Runtime.NODEJS_18_X,
-            handler: "hello.handler",
-            code: Code.fromAsset("lambda"),
-        }),
+// 共通のダウンストリームLambda関数を作成する関数
+const createTestLambdaFunction = (stack: Stack): Function =>
+    new Function(stack, "TestFunction", {
+        runtime: Runtime.NODEJS_18_X,
+        handler: "hello.handler",
+        code: Code.fromAsset("lambda"),
     });
-    // THEN
-    const template = Template.fromStack(stack);
-    template.hasResourceProperties("AWS::DynamoDB::Table", {
-        SSESpecification: {
-            SSEEnabled: true,
-        },
+
+// 各テストケース
+describe('HitCounter', () => {
+    let stack: Stack;
+    let testLambdaFunction: Function;
+
+    // 各テストケースの前に実行される
+    beforeEach(() => {
+        stack = new Stack();
+        testLambdaFunction = createTestLambdaFunction(stack);
     });
-});
 
-test("read capacity can be configured", () => {
-    const stack = new Stack();
-
-    expect(() => {
+    test("DynamoDB Table Created With Encryption", () => {
         new HitCounter(stack, "MyTestConstruct", {
-            downstream: new Function(stack, "TestFunction", {
-                runtime: Runtime.NODEJS_18_X,
-                handler: "hello.handler",
-                code: Code.fromAsset("lambda"),
-            }),
-            readCapacity: 3,
+            downstream: testLambdaFunction,
         });
-    }).toThrow(/readCapacity must be greater than 5 and less than 20/);
-});
 
-test("Lambda Has Environment Variables", () => {
-    const stack = new Stack();
-    // WHEN
-    new HitCounter(stack, "MyTestConstruct", {
-        downstream: new Function(stack, "TestFunction", {
-            runtime: Runtime.NODEJS_18_X,
-            handler: "hello.handler",
-            code: Code.fromAsset("lambda"),
-        }),
-    });
-    // THEN
-    const template = Template.fromStack(stack);
-    const envCapture = new Capture();
-    template.hasResourceProperties("AWS::Lambda::Function", {
-        Environment: envCapture,
+        const template = Template.fromStack(stack);
+        template.hasResourceProperties("AWS::DynamoDB::Table", {
+            SSESpecification: {
+                SSEEnabled: true,
+            },
+        });
     });
 
-    expect(envCapture.asObject()).toEqual({
-        Variables: {
-            DOWNSTREAM_FUNCTION_NAME: {
-                Ref: "TestFunction22AD90FC",
+    test("read capacity can be configured", () => {
+        expect(() => {
+            new HitCounter(stack, "MyTestConstruct", {
+                downstream: testLambdaFunction,
+                readCapacity: 3,
+            });
+        }).toThrow(/readCapacity must be greater than 5 and less than 20/);
+    });
+
+    test("Lambda Has Environment Variables", () => {
+        new HitCounter(stack, "MyTestConstruct", {
+            downstream: testLambdaFunction,
+        });
+
+        const template = Template.fromStack(stack);
+        const envCapture = new Capture();
+        template.hasResourceProperties("AWS::Lambda::Function", {
+            Environment: envCapture,
+        });
+
+        expect(envCapture.asObject()).toEqual({
+            Variables: {
+                DOWNSTREAM_FUNCTION_NAME: {
+                    Ref: "TestFunction22AD90FC",
+                },
+                HITS_TABLE_NAME: {
+                    Ref: "MyTestConstructHits24A357F0",
+                },
             },
-            HITS_TABLE_NAME: {
-                Ref: "MyTestConstructHits24A357F0",
-            },
-        },
+        });
     });
 });
