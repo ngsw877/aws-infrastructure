ae49da4026cb3920c9600560442e193b6702b825
ngsw877 <otsda1055@yahoo.co.jp>
2024-09-17 08:55:03 +0900

    add: 定期バッチ用リソースのためのCDKプロジェクトを作成（cdk initのみ）

---
+++ CDK/scheduled-batch/test/scheduled-batch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ScheduledBatch from '../lib/scheduled-batch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/scheduled-batch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ScheduledBatch.ScheduledBatchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
