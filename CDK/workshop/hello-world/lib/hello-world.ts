caf2818370b8b16d3694c8cdd3520011f032ee33
ngsw877 <otsda1055@yahoo.co.jp>
2024-09-16 16:08:22 +0900

    refactor: Lambda関数を管理するディレクトリ構成を変更

--- CDK/workshop/hello-world/lib/hello-world.ts
+++ CDK/workshop/hello-world/lib/hello-world.ts
@@ -1,11 +1,16 @@
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
 export class HelloWorld extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
-        const helloFunction = new NodejsFunction(this, 'function');
+        
+        const helloFunction = new NodejsFunction(this, 'function', {
+            entry: path.join(__dirname, '..', 'lambda', 'hallo-world', 'index.ts'),
+        });
+        
         new LambdaRestApi(this, 'apigw', {
             handler: helloFunction,
         });
