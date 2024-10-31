20284dd55be666a7b1ccf3e6bb0afacddc6204ed
ngsw877 <otsda1055@yahoo.co.jp>
2024-10-31 10:09:17 +0900

    udpate

--- SAM/workshop/apigw_lambda_dynamodb/src/app.py
+++ SAM/workshop/apigw_lambda_dynamodb/src/app.py
@@ -1,9 +1,18 @@
+import os
 import json
 import boto3
-dynamodb_client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    dynamodb_client.put_item(TableName='SampleTable', Item={'id': {'S': '1'}, 'value': {'S': 'Value1'}})
+    table_name = os.environ['TABLE_NAME'] 
+    dynamodb_client = boto3.client('dynamodb')
+    dynamodb_client.put_item(
+        TableName=table_name,
+        Item={
+            'id': {'S': '1'},
+            'value': {'S': 'Value1'}
+        }
+    )
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
