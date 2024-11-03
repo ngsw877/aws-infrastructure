03ee54445409eeeaa98e56e030f67098953ce62a
ngsw877 <otsda1055@yahoo.co.jp>
2024-11-03 22:17:15 +0900

    update: スクリプト改修

--- SAM/alternate-contacts/loop-through-accounts.sh
+++ SAM/alternate-contacts/loop-through-accounts.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
-managementaccount=`aws organizations describe-organization --query Organization.MasterAccountId --output text`
 
-for account in $(aws organizations list-accounts --query 'Accounts[].Id' --output text); do
-    if [ "$managementaccount" -eq "$account" ]; then
+master_account_profile="default"
+
+management_account=`aws organizations describe-organization --profile $master_account_profile --query Organization.MasterAccountId --output text`
+
+for account in $(aws organizations list-accounts --profile $master_account_profile --query 'Accounts[].Id' --output text); do
+    # 管理アカウントはスキップ
+    # NOTE: 管理アカウントの代替連絡先の登録に、aws account put-alternate-contactコマンドを使用するとエラーが発生するため、管理アカウントでログインしマネジメントコンソールから登録する等他の方法を使用する
+    if [ "$management_account" -eq "$account" ]; then
         echo 'Skipping management account.'
         continue
     fi
-    ./put-security-contact.sh -a $account
+    ./put-security-contact.sh $master_account_profile $account
     sleep 0.2
 done
--- SAM/alternate-contacts/put-security-contact.sh
+++ SAM/alternate-contacts/put-security-contact.sh
@@ -1,11 +1,28 @@
 #! /bin/bash
-while getopts a: flag
-do
-    case "${flag}" in
-        a) account_id=${OPTARG};;
-    esac
-done
+
+profile=$1
+account_id=$2
+
+### 代替連絡先の設定（全AWSアカウント共通） #################
+# 氏名
+name="Mary Major"
+# 役職
+title="Security Contact"
+# Eメール
+email_address="security-contact@example.com"
+# 電話番号
+phone_number="+1(555)555-5555"
+#######################################################
 
 echo 'Put security contact for account '$account_id'...'
-aws account put-alternate-contact   --account-id $account_id   --alternate-contact-type=SECURITY   --email-address=security-contact@example.com   --phone-number="+1(555)555-5555"   --title="Security Contact"   --name="Mary Major"
+
+aws account put-alternate-contact \
+    --profile "$profile" \
+    --account-id "$account_id" \
+    --alternate-contact-type=SECURITY \
+    --name="$name" \
+    --title="$title" \
+    --email-address="$email_address" \
+    --phone-number="$phone_number"
+
 echo 'Done putting security contact for account '$account_id'.'
