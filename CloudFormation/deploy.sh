9ec10103cb9016af7939cf9ae4fd9bd984941160
ngsw877 <otsda1055@yahoo.co.jp>
2024-11-04 22:45:46 +0900

    update: CFnデプロイの際、オプションで変更セットをそのまま実行して直接デプロイできるようにした

--- CloudFormation/deploy.sh
+++ CloudFormation/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 引数の数をチェック
-if [ "$#" -eq 0 ]; then
+if [ "$#" -lt 3 ]; then
   echo "profileを指定してください"
   read -r PROFILE
 
@@ -12,9 +12,17 @@ if [ "$#" -eq 0 ]; then
   read -r STACK_NAME
 else
   # コマンドライン引数から値を割り当て
-  PROFILE=$1
-  TEMPLATE=$2
-  STACK_NAME=$3
+  PROFILE="$1"
+  TEMPLATE="$2"
+  STACK_NAME="$3"
+fi
+
+# デフォルトで --no-execute-changeset オプションを設定
+changeset_option="--no-execute-changeset"
+
+# 引数に -e(execute) が指定されている場合、オプションを外す
+if [[ "$4" == "-e" ]]; then
+  changeset_option=""
 fi
 
 # AWS CloudFormationを使用してデプロイ
@@ -24,4 +32,4 @@ aws cloudformation \
   --template-file "${TEMPLATE}" \
   --stack-name "${STACK_NAME}"  \
   --capabilities CAPABILITY_NAMED_IAM \
-  --no-execute-changeset
+  $changeset_option
