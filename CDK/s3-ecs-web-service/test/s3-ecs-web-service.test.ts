29de85292d512fefa172b4e879fd412e965a1ffd
ngsw877 <otsda1055@yahoo.co.jp>
2024-11-09 17:22:56 +0900

    add: 新CDKプロジェクト作成

---
+++ CDK/s3-ecs-web-service/test/s3-ecs-web-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3EcsWebService from '../lib/s3-ecs-web-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-ecs-web-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3EcsWebService.S3EcsWebServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
