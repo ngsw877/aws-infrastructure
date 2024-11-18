19ca8d29ea1799d4ef7243e769d1d8edf1006696
ngsw877 <otsda1055@yahoo.co.jp>
2024-11-18 18:01:04 +0900

    refactor: 変数を外部ファイル化

--- SAM/alternate-contacts/put-sec-alt-contacts-for-all-member-accounts.sh
+++ SAM/alternate-contacts/put-sec-alt-contacts-for-all-member-accounts.sh
@@ -1,40 +1,40 @@
 #! /bin/bash
 
-master_account_profile="default"
+# 環境変数の読み込み
+source ./security_contact_config.sh
 
-### 代替連絡先の設定（全AWSアカウント共通） #################
-# 氏名
-name="Mary Major" 
-# 役職
-title="Security Contact"
-# Eメール 
-email_address="security-contact@example.com"
-# 電話番号
-phone_number="+1(555)555-5555"
-#######################################################
+management_account=$(
+    aws organizations describe-organization \
+    --profile "${MASTER_ACCOUNT_PROFILE}" \
+    --query Organization.MasterAccountId \
+    --output text
+)
 
-management_account=`aws organizations describe-organization --profile $master_account_profile --query Organization.MasterAccountId --output text`
-
-for account in $(aws organizations list-accounts --profile $master_account_profile --query 'Accounts[].Id' --output text); do
+for account in $(
+    aws organizations list-accounts \
+        --profile "${MASTER_ACCOUNT_PROFILE}" \
+        --query 'Accounts[].Id' \
+        --output text
+); do
     # 管理アカウントはスキップ 
     # NOTE: 管理アカウントの代替連絡先の登録に、aws account put-alternate-contactコマンドを使用するとエラーが発生するため、管理アカウントでログインしマネジメントコンソールから登録する等他の方法を使用する
-    if [ "$management_account" -eq "$account" ]; then
+    if [ "${management_account}" -eq "${account}" ]; then
         echo '管理アカウントをスキップします。'
         continue
     fi
 
-    echo 'アカウント'$account'の代替連絡先を設定します...'
+    echo "アカウント${account}の代替連絡先を設定します..."
 
     aws account put-alternate-contact \
-        --profile "$master_account_profile" \
-        --account-id "$account" \
+        --profile "${MASTER_ACCOUNT_PROFILE}" \
+        --account-id "${account}" \
         --alternate-contact-type=SECURITY \
-        --name="$name" \
-        --title="$title" \
-        --email-address="$email_address" \
-        --phone-number="$phone_number"
+        --name="${SECURITY_CONTACT_NAME}" \
+        --title="${SECURITY_CONTACT_TITLE}" \
+        --email-address="${SECURITY_CONTACT_EMAIL}" \
+        --phone-number="${SECURITY_CONTACT_PHONE}"
 
-    echo 'アカウント'$account'の代替連絡先の設定が完了しました。'
+    echo "アカウント${account}の代替連絡先の設定が完了しました。"
     
     sleep 0.2
 done 
---
+++ SAM/alternate-contacts/security_contact_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# マスターアカウントのプロファイル設定
+export MASTER_ACCOUNT_PROFILE="default"
+
+### セキュリティ代替連絡先の設定（全AWSアカウント共通） #################
+# 氏名
+export SECURITY_CONTACT_NAME="Mary Major" 
+# 役職
+export SECURITY_CONTACT_TITLE="Security Contact"
+# Eメール 
+export SECURITY_CONTACT_EMAIL="security-contact@example.com"
+# 電話番号
+export SECURITY_CONTACT_PHONE="+1(555)555-5555"
