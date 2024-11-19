b4c40f6d5ad0c33ae121b7f75216362f536216f0
ngsw877 <otsda1055@yahoo.co.jp>
2024-11-19 22:48:25 +0900

    add: 指定したSlackチャンネルのメッセージを取得するLambdaを作成

---
+++ CDK/lambda-collection/lambda/get-slack-message/index.ts
@@ -0,0 +1,58 @@
+import { WebClient } from "@slack/web-api";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import { getParameterStoreValue } from "../../../scheduled-batch/lambda/common/ssm";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const SLACK_CHANNEL_ID_PARAMETER_STORE_NAME = process.env.SLACK_CHANNEL_ID_PARAMETER_STORE_NAME;
+const SLACK_TOKEN_PARAMETER_STORE_NAME = process.env.SLACK_TOKEN_PARAMETER_STORE_NAME;
+
+if (!SLACK_CHANNEL_ID_PARAMETER_STORE_NAME || !SLACK_TOKEN_PARAMETER_STORE_NAME) {
+  throw new Error("必要な環境変数が設定されていません");
+}
+
+export const handler = async () => {
+  try {
+    const slackToken = await getParameterStoreValue(SLACK_TOKEN_PARAMETER_STORE_NAME);
+    const slackChannelId = await getParameterStoreValue(SLACK_CHANNEL_ID_PARAMETER_STORE_NAME);
+
+    const slack = new WebClient(slackToken);
+
+    // 現在から1時間前のUNIXタイムスタンプを取得
+    const oneHourAgoUnixTimestamp = dayjs().tz("Asia/Tokyo").subtract(1, 'hour').unix().toString();
+
+    // 指定したSlackチャンネルの、1時間前以降のメッセージを取得
+    // API仕様書: https://api.slack.com/methods/conversations.history
+    const result = await slack.conversations.history({
+      channel: slackChannelId,
+      oldest: oneHourAgoUnixTimestamp,
+    });
+
+    // messagesが存在するか確認
+    if (result.messages && Array.isArray(result.messages)) {
+      const messages = result.messages.map((msg) => ({
+        text: msg.text,
+        formattedDate: dayjs.unix(msg.ts ? Number.parseFloat(msg.ts) : 0).tz("Asia/Tokyo").format('YYYY-MM-DD HH:mm:ss'),
+      }));
+      return {
+        statusCode: 200,
+        body: {
+          messages,
+        },
+      };
+    }
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: "メッセージが取得できませんでした。" }),
+    };
+  } catch (error) {
+    console.error("エラー:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "エラーが発生しました。" }),
+    };
+  }
+};
--- CDK/lambda-collection/lib/lambda-collection-stack.ts
+++ CDK/lambda-collection/lib/lambda-collection-stack.ts
@@ -1,16 +1,54 @@
-import * as cdk from "aws-cdk-lib";
+import { 
+  Stack, 
+  type StackProps, 
+  RemovalPolicy, 
+  aws_iam as iam,
+  aws_lambda as lambda,
+  aws_logs as logs,
+} from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import type { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from "node:path";
 
-export class LambdaCollectionStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class LambdaCollectionStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const SLACK_CHANNEL_ID_PARAMETER_STORE_NAME = "/test_app/slack_channel_id";
+    const SLACK_TOKEN_PARAMETER_STORE_NAME = "/test_app/slack_token";
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCollectionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+        // Lambda
+        const getSlackMessageLambda = new NodejsFunction(
+          this,
+          "GetSlackMessageLambda",
+          {
+            entry: path.join(
+              __dirname,
+              "../lambda/get-slack-message/index.ts",
+            ),
+            handler: "handler",
+            runtime: lambda.Runtime.NODEJS_20_X,
+            environment: {
+              SLACK_CHANNEL_ID_PARAMETER_STORE_NAME,
+              SLACK_TOKEN_PARAMETER_STORE_NAME,
+            },
+          },
+        );
+
+        // 権限を追加
+        getSlackMessageLambda.addToRolePolicy(new iam.PolicyStatement({
+          actions: ["ssm:GetParameter"],
+          resources: [
+            `arn:${this.partition}:ssm:${this.region}:${this.account}:parameter${SLACK_CHANNEL_ID_PARAMETER_STORE_NAME}`,
+            `arn:${this.partition}:ssm:${this.region}:${this.account}:parameter${SLACK_TOKEN_PARAMETER_STORE_NAME}`,
+          ],
+        }));
+    
+            // Lambdaのロググループ
+            new logs.LogGroup(this, `${getSlackMessageLambda.node.id}LogGroup`, {
+              logGroupName: `/aws/lambda/${getSlackMessageLambda.functionName}`,
+              retention: logs.RetentionDays.THREE_MONTHS,
+              removalPolicy: RemovalPolicy.DESTROY,
+            });
   }
 }
