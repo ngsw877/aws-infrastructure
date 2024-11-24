c23a41d021fd095ed20e5f39049d7f9b83e6a259
ngsw877 <otsda1055@yahoo.co.jp>
2024-11-24 21:26:49 +0900

    add: デフォルトレイアウトを作成

---
+++ demo-application/frontend/nuxt/src/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <slot />
+    <footer>
+      <p>Copyright &copy; 2024 ngsw877. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+<style scoped>
+.container {
+  color: red;
+}
+
+footer {
+  background-color: orange;
+  color: white;
+  text-align: center;
+}
+</style>
