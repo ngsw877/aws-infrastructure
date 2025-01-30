2ae080f84d0e1d39c3a3dde32c47de66ecd27040
ngsw877 <otsda1055@yahoo.co.jp>
2025-01-30 20:35:25 +0900

    add: 既存のタスク定義情報を取得し、新しいタスク定義を登録するためのコマンドを追加

--- scripts/commands/aws_cli/ecs.sh
+++ scripts/commands/aws_cli/ecs.sh
@@ -2,14 +2,14 @@
 
 # FARGATEタスクを起動する
 cluster_name=
-task_definition=
+task_def_family=
 subnet_id=
 security_group_id=
 
 aws ecs run-task \                                                                                      
   --cluster ${cluster_name} \          
   --launch-type FARGATE \             
-  --task-definition ${task_definition} \
+  --task-definition ${task_def_family} \
   --network-configuration "awsvpcConfiguration={subnets=[${subnet_id}],securityGroups=[${security_group_id}],assignPublicIp=DISABLED}"
 
 ## FARGATEタスクのアプリコンテナで任意のコマンドを実行し、タスクARNを取得する
@@ -18,7 +18,7 @@ aws ecs run-task \
 task_arn=$(aws ecs run-task \                                                                                      
   --cluster ${cluster_name} \          
   --launch-type FARGATE \             
-  --task-definition ${task_definition} \
+  --task-definition ${task_def_family} \
   --network-configuration "awsvpcConfiguration={subnets=[${subnet_id}],securityGroups=[${security_group_id}],assignPublicIp=DISABLED}" \
   --overrides '{
     "containerOverrides": [
@@ -43,10 +43,10 @@ aws ecs describe-tasks \
 
 ## アプリコンテナの終了コードを取得する
 app_exit_code=$(aws ecs describe-tasks \
-            --cluster ${cluster_name} \
-            --tasks ${task_arn} \
-            --query 'tasks[0].containers[?name==`app`].exitCode' \
-            --output text)
+  --cluster ${cluster_name} \
+  --tasks ${task_arn} \
+  --query 'tasks[0].containers[?name==`app`].exitCode' \
+  --output text)
 
 # タスクが終了するまで待機する
 cluster_name=
@@ -55,3 +55,42 @@ task_arn=
 aws ecs wait tasks-stopped \
   --cluster ${cluster_name} \
   --tasks ${task_arn}
+
+# タスク定義情報を取得
+task_def_family=
+
+task_def=$(aws ecs describe-task-definition \
+  --task-definition $task_def_family \
+  --query 'taskDefinition' \
+  --output json)
+
+echo "${task_def}" | pbcopy
+
+## 取得したタスク定義情報をもとに、パラメータの上書きと整形を行い新しいタスク定義用jsonを作成する
+## 参考: https://qiita.com/moko_Swallows/items/f49052832e8db24286b8
+image=
+task_def_file_name="taskdef.json"
+
+new_task_def=$(
+  echo "${task_def}" |
+  jq --arg IMAGE "${image}" \
+  'del(
+      .taskDefinitionArn,
+      .revision,
+      .status,
+      .requiresAttributes,
+      .compatibilities,
+      .registeredAt,
+      .registeredBy
+    ) |
+    .containerDefinitions[0].image = $IMAGE
+  ')
+
+echo "${new_task_def}" > ${task_def_file_name}
+
+## 新しいタスク定義を作成する
+new_task_def_family="new-task-def"
+
+aws ecs register-task-definition \
+  --family "${new_task_def_family}" \
+  --cli-input-json file://${task_def_file_name}
