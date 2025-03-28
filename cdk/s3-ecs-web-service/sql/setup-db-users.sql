902d1d5b6eb500a704d33a41d483e11bf8a4a7ea
ngsw877 <otsda1055@yahoo.co.jp>
2025-03-28 16:36:14 +0900

    feat: データベースユーザー削除クエリを追加

--- cdk/s3-ecs-web-service/sql/setup-db-users.sql
+++ cdk/s3-ecs-web-service/sql/setup-db-users.sql
@@ -1,3 +1,5 @@
+/* ユーザー作成セクション */
+
 -- readonly_userユーザーを作成
 CREATE USER readonly_user WITH PASSWORD 'ここにSecrets Managerから取得したパスワード';
 -- データベースに接続できるようにする
@@ -5,10 +7,23 @@ GRANT CONNECT ON DATABASE "sample_app" TO readonly_user;
 -- readonly_userユーザーに読み取り権限を付与
 GRANT pg_read_all_data TO readonly_user;
 
-
 -- readwrite_userユーザーを作成
 CREATE USER readwrite_user WITH PASSWORD 'ここにSecrets Managerから取得したパスワード';
 -- データベースに接続できるようにする
 GRANT CONNECT ON DATABASE "sample_app" TO readwrite_user;
 -- readwrite_userユーザーに読み取り権限と書き込み権限を付与
 GRANT pg_read_all_data, pg_write_all_data TO readwrite_user;
+
+
+/* ユーザー削除セクション */
+
+-- readonly_userの権限を剥奪し、ユーザーを削除
+REVOKE CONNECT ON DATABASE sample_app FROM readonly_user;
+DROP USER readonly_user;
+
+-- readwrite_userの権限を剥奪し、ユーザーを削除
+REVOKE CONNECT ON DATABASE sample_app FROM readwrite_user;
+DROP USER readwrite_user;
+
+-- 現在のユーザー一覧を表示（デバッグ用）
+SELECT usename FROM pg_user;
