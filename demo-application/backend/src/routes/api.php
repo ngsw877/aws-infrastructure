961e65610f163df03b87ccd937178c71c070a5c0
ngsw877 <otsda1055@yahoo.co.jp>
2025-07-03 16:04:01 +0900

    feat: マルチテナント対応のドメイン識別機能とUI改善
      - Laravel側:
      ドメイン取得APIエンドポイント(/api/domain)を追加
      - Nuxt側:
      ドメイン表示機能とSCSSベースの新デザインを実装
      - リクエストドメインを自動識別してページに表示す
      る機能を追加

--- demo-application/backend/src/routes/api.php
+++ demo-application/backend/src/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HealthCheckController;
+use App\Http\Controllers\DomainController;
 
 // ヘルスチェックAPI
 Route::get('/health_check', [HealthCheckController::class, 'healthCheck']);
+
+// ドメイン取得API
+Route::get('/domain', [DomainController::class, 'getCurrentDomain']);
