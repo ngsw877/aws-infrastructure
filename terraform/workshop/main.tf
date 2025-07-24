225169eafc07fe64b23d01470bcae9eb54705920
ngsw877 <otsda1055@yahoo.co.jp>
2025-07-24 21:43:57 +0900

    feat: EC2インスタンスにセキュリティグループを追加
      - SSH（22番ポート）とHTTP（80番ポート）のインバウンドルール
      を設定
      - 全てのアウトバウンドトラフィックを許可
      - セキュリティグループをEC2インスタンスに関連付け

--- terraform/workshop/main.tf
+++ terraform/workshop/main.tf
@@ -1,9 +1,40 @@
 resource "aws_instance" "this" {
   ami           = "ami-07b3f199a3bed006a" # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.this.id]
 
   tags = {
     Name = "workshop-instance"
   }
+}
 
+resource "aws_security_group" "this" {
+  name = "workshop-ec2-sg"
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
 }
