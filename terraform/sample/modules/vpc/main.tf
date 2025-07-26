174f531e39f4a123c15e5c9c659e8fdbe495c938
ngsw877 <otsda1055@yahoo.co.jp>
2025-07-26 10:51:48 +0900

    fix: countではなくfor_eachでサブネットを作成するよう修正

--- terraform/sample/modules/vpc/main.tf
+++ terraform/sample/modules/vpc/main.tf
@@ -1,14 +1,9 @@
 locals {
-  availability_zones = [
-    "ap-northeast-1a",
-    "ap-northeast-1c",
-    "ap-northeast-1d"
-  ]
-  subnet_cidr_blocks = [
-    "10.0.0.0/24",
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
+  subnet_args = {
+    "ap-northeast-1a" = "10.0.0.0/24"
+    "ap-northeast-1c" = "10.0.1.0/24"
+    "ap-northeast-1d" = "10.0.2.0/24"
+  }
 }
 
 resource "aws_vpc" "this" {
@@ -20,10 +15,10 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(local.availability_zones)
+  for_each = local.subnet_args
 
   vpc_id     = aws_vpc.this.id
-  cidr_block = local.subnet_cidr_blocks[count.index]
-  availability_zone = local.availability_zones[count.index]
+  cidr_block = each.value
+  availability_zone = each.key
 
 }
