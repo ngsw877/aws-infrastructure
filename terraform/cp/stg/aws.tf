d0ecb6326c7e917e11888d2fb99f477e56f8a318
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-11-29 16:29:46 +0900

    feat: ECRを１つ追加

--- terraform/cp/stg/aws.tf
+++ terraform/cp/stg/aws.tf
@@ -1,3 +1,7 @@
 resource "aws_ecr_repository" "sample" {
   name = "cp-test-ecr-stg"
 }
+
+resource "aws_ecr_repository" "sample_2" {
+  name = "cp-test-ecr-2-stg"
+}
--- terraform/cp/stg/backend.tf
+++ terraform/cp/stg/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "cp-terraform-ngsw-stg"
-    key    = "main.tfstate"
-    region = "ap-northeast-1"
+    bucket  = "cp-terraform-ngsw-stg"
+    key     = "main.tfstate"
+    region  = "ap-northeast-1"
+    profile = "study"
   }
 }
