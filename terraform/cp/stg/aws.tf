099efb26dbe50db02b255e80b984cb5525fb8a19
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-11-30 23:27:05 +0900

    feat: サブネットを１つimport

--- terraform/cp/stg/aws.tf
+++ terraform/cp/stg/aws.tf
@@ -15,3 +15,16 @@ module "vpc" {
   env    = "stg"
 }
 
+import {
+  to = aws_subnet.tmp
+  id = "subnet-0ab04c255c12ed5f3"
+}
+
+resource "aws_subnet" "tmp" {
+  cidr_block        = "10.0.128.0/18"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "private-subnet-1a-stg"
+  }
+  vpc_id = "vpc-092b7ae2d585fb553"
+}
