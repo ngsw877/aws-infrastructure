1e4eeed27790733a07b2aa8a4a6dc7f62ee4c4c8
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-11-30 15:09:42 +0900

    reafactor: モジュール化

--- terraform/cp/stg/aws.tf
+++ terraform/cp/stg/aws.tf
@@ -1,49 +1,11 @@
-resource "aws_ecr_repository" "sample" {
+module "ecr_sample" {
+  source               = "../modules/aws/ecr_unit"
   name                 = "cp-test-ecr-stg"
   image_tag_mutability = "IMMUTABLE"
 }
 
-resource "aws_ecr_lifecycle_policy" "lifecycle_policy_sample" {
-  repository = aws_ecr_repository.sample.name
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "最新の3つのイメージのみを保持"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 3
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_ecr_repository" "sample_2" {
+module "ecr_sample_2" {
+  source               = "../modules/aws/ecr_unit"
   name                 = "cp-test-ecr-2-stg"
   image_tag_mutability = "IMMUTABLE"
-}
-
-resource "aws_ecr_lifecycle_policy" "lifecycle_policy_sample_2" {
-  repository = aws_ecr_repository.sample_2.name
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "最新の3つのイメージのみを保持"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 3
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
 }
