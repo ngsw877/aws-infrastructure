683ebe3294cd156c1c8cde662ccba9aeec42419d
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-11-30 12:08:54 +0900

    feat: update ECR

--- terraform/cp/stg/aws.tf
+++ terraform/cp/stg/aws.tf
@@ -3,13 +3,47 @@ resource "aws_ecr_repository" "sample" {
   image_tag_mutability = "IMMUTABLE"
 }
 
+resource "aws_ecr_lifecycle_policy" "lifecycle_policy_sample" {
+  repository = aws_ecr_repository.sample.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "最新の3つのイメージのみを保持"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_ecr_repository" "sample_2" {
   name                 = "cp-test-ecr-2-stg"
   image_tag_mutability = "IMMUTABLE"
 }
 
-resource "aws_ecr_repository" "sample_3" {
-  count                = 1
-  name                 = "cp-test-ecr-3-stg"
-  image_tag_mutability = "MUTABLE"
+resource "aws_ecr_lifecycle_policy" "lifecycle_policy_sample_2" {
+  repository = aws_ecr_repository.sample_2.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "最新の3つのイメージのみを保持"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
