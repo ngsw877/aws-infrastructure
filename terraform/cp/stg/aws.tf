c30390525baf97558cbb7f813363f97aadf801d7
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-11-30 12:05:23 +0900

    feat: update ECR

--- terraform/cp/stg/aws.tf
+++ terraform/cp/stg/aws.tf
@@ -1,3 +1,15 @@
 resource "aws_ecr_repository" "sample" {
-  name = "cp-test-ecr-stg"
+  name                 = "cp-test-ecr-stg"
+  image_tag_mutability = "IMMUTABLE"
+}
+
+resource "aws_ecr_repository" "sample_2" {
+  name                 = "cp-test-ecr-2-stg"
+  image_tag_mutability = "IMMUTABLE"
+}
+
+resource "aws_ecr_repository" "sample_3" {
+  count                = 1
+  name                 = "cp-test-ecr-3-stg"
+  image_tag_mutability = "MUTABLE"
 }
