24027f4e7c9e0381ff50cb62d34ab7fb4c754fe5
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-12-01 00:11:30 +0900

    feat: 3つのサブネットをモジュールとしてインポート

--- terraform/cp/modules/aws/subnet/main.tf
+++ terraform/cp/modules/aws/subnet/main.tf
@@ -6,3 +6,32 @@ resource "aws_subnet" "private_subnet_1a" {
   }
   vpc_id = var.vpc_id
 }
+
+resource "aws_subnet" "private_subnet_1c" {
+  cidr_block        = "10.0.192.0/18"
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    Name = "private-subnet-1c-${var.env}"
+  }
+  vpc_id = var.vpc_id
+}
+
+resource "aws_subnet" "public_subnet_1a" {
+  cidr_block              = "10.0.0.0/18"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1a-${var.env}"
+  }
+  vpc_id = var.vpc_id
+}
+
+resource "aws_subnet" "public_subnet_1c" {
+  cidr_block              = "10.0.64.0/18"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1c-${var.env}"
+  }
+  vpc_id = var.vpc_id
+}
