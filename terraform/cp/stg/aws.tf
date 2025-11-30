c816d9b27ff32b989a2be83ece1bfb074715c759
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-11-30 22:53:37 +0900

    feat: VPCをimport

--- terraform/cp/stg/aws.tf
+++ terraform/cp/stg/aws.tf
@@ -1,11 +1,24 @@
-module "ecr_sample" {
-  source               = "../modules/aws/ecr_unit"
-  name                 = "cp-test-ecr-stg"
-  image_tag_mutability = "IMMUTABLE"
+# module "ecr_sample" {
+#   source               = "../modules/aws/ecr_unit"
+#   name                 = "cp-test-ecr-stg"
+#   image_tag_mutability = "IMMUTABLE"
+# }
+
+# module "ecr_sample_2" {
+#   source               = "../modules/aws/ecr_unit"
+#   name                 = "cp-test-ecr-2-stg"
+#   image_tag_mutability = "IMMUTABLE"
+# }
+
+import {
+  to = aws_vpc.tmp
+  id = "vpc-092b7ae2d585fb553"
 }
 
-module "ecr_sample_2" {
-  source               = "../modules/aws/ecr_unit"
-  name                 = "cp-test-ecr-2-stg"
-  image_tag_mutability = "IMMUTABLE"
+resource "aws_vpc" "tmp" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "cp-stg"
+  }
 }
+
