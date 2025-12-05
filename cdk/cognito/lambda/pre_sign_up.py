7b5286f90f6583dabdb4dd1dfa6f5c4416e00345
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-12-05 18:18:30 +0900

    feat: シンプルなCognitoスタックをCDKで作成

---
+++ cdk/cognito/lambda/pre_sign_up.py
@@ -0,0 +1,4 @@
+# PreSignUpAutoConfirm
+def lambda_handler(event, context):
+    event['response']['autoConfirmUser'] = True
+    return event
---
+++ cdk/cognito/lib/cognito-stack.ts
@@ -0,0 +1,72 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as path from 'path';
+
+export class CognitoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    // Cognito User Pool
+    const userPool = new cognito.UserPool(this, "MyUserPool", {
+      userPoolName: "MyUserPool",
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: false,
+        username: true,
+      },
+      standardAttributes: {
+        email: {
+          required: false,
+          mutable: true,
+        },
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: true,
+      },
+      accountRecovery: cognito.AccountRecovery.NONE,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      mfa: cognito.Mfa.OFF,
+      lambdaTriggers: {
+        preSignUp: new lambda.Function(this, "PreSignUpAutoConfirmFunction", {
+          runtime: lambda.Runtime.PYTHON_3_13,
+          handler: 'pre_sign_up.lambda_handler',
+          code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+          timeout: cdk.Duration.seconds(3),
+        }),
+      },
+    });
+
+    // User Pool Client
+    new cognito.UserPoolClient(
+      this,
+      "MyUserPoolClient",
+      {
+        userPool,
+        generateSecret: true,
+        authFlows: {
+          adminUserPassword: true,
+          userPassword: true,
+          userSrp: true,
+        },
+        accessTokenValidity: cdk.Duration.days(1),
+      }
+    );
+
+    // Cognito用のポリシー
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        'cognito-idp:AdminSetUserPassword',
+      ],
+      resources: [userPool.userPoolArn],
+    });
+
+  }
+}
