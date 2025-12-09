619762de39039b1df062263419420c50094ce9c5
ngsw877 <58071320+ngsw877@users.noreply.github.com>
2025-12-09 18:01:41 +0900

    feat: site2用パラメータとメンテナンスページを追加

---
+++ cdk/maintenance-page/params/site2.ts
@@ -0,0 +1,7 @@
+import {SiteParams} from '../types/params';
+
+export const params: SiteParams = {
+  domainName: 'hoge-app.click',
+  hostedZoneId: 'Z09351231EZC2KL14JWB6',
+  webpagePath: './webpage/site2',
+};
