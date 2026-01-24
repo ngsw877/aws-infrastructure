656c2429c0a1735775f92fd3cd8bcd7f5617bd45
nagasawa shuntaro <shuntaro@nagasawanoMac-mini.local>
2026-01-24 17:16:54 +0900

    feat: IGWをapply(prd)

--- terraform/cp/prd/aws.tf
+++ terraform/cp/prd/aws.tf
@@ -8,3 +8,9 @@ module "subnet" {
   env    = local.env
   vpc_id = module.vpc.id_cp
 }
+
+module "internet_gateway" {
+  source = "../modules/aws/internet_gateway"
+  env    = local.env
+  vpc_id = module.vpc.id_cp
+}
