5d7a164cbf62bbbe3b3da2e4fe757b71fc4d7229
nagasawa shuntaro <shuntaro@nagasawanoMac-mini.local>
2026-01-24 17:00:50 +0900

    feat: サブネットをapply(prd)

--- terraform/cp/prd/aws.tf
+++ terraform/cp/prd/aws.tf
@@ -2,3 +2,9 @@ module "vpc" {
   source = "../modules/aws/vpc"
   env    = local.env
 }
+
+module "subnet" {
+  source = "../modules/aws/subnet"
+  env    = local.env
+  vpc_id = module.vpc.id_cp
+}
