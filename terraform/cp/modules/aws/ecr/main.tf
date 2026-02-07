f3271da53b2207d8aa0cb24d10d51f102c9e7919
nagasawa shuntaro <shuntaro@nagasawanoMac-mini.local>
2026-02-07 20:23:03 +0900

    feat: cost-aggregator,cost-providerののECRを作成

--- terraform/cp/modules/aws/ecr/main.tf
+++ terraform/cp/modules/aws/ecr/main.tf
@@ -7,3 +7,13 @@ module "ecr_slack_metrics" {
   source = "../ecr_unit"
   name   = "slack-metrics-${var.env}"
 }
+
+module "ecr_cost_aggregator" {
+  source = "../ecr_unit"
+  name   = "cost-aggregator-${var.env}"
+}
+
+module "ecr_cost_provider" {
+  source = "../ecr_unit"
+  name   = "cost-provider-${var.env}"
+}
