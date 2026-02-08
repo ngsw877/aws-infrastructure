24538377aeae954904a3d9be6192cab7431a0734
nagasawa shuntaro <shuntaro@nagasawanoMac-mini.local>
2026-02-08 16:56:02 +0900

    feat: cost-api用のターゲットグループを作成

--- terraform/cp/modules/aws/target_group/main.tf
+++ terraform/cp/modules/aws/target_group/main.tf
@@ -18,3 +18,27 @@ resource "aws_lb_target_group" "slack_metrics_api" {
     unhealthy_threshold = 2
   }
 }
+
+/************************************************************
+Datadogコースで使用
+************************************************************/
+resource "aws_lb_target_group" "cost_api" {
+  vpc_id               = var.vpc_id
+  name                 = "cost-api-${var.env}"
+  deregistration_delay = "115"
+  port                 = 80
+  protocol             = "HTTP"
+  target_type          = "ip"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 10
+    matcher             = "200"
+    path                = "/api/health"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
--- terraform/cp/modules/aws/target_group/outputs.tf
+++ terraform/cp/modules/aws/target_group/outputs.tf
@@ -1,3 +1,7 @@
 output "arn_slack_metrics_api" {
   value = aws_lb_target_group.slack_metrics_api.arn
 }
+
+output "arn_cost_api" {
+  value = aws_lb_target_group.cost_api.arn
+}
